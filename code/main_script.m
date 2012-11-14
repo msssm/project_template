084069783fbd5dada62dff26fcfd354bc8140c86
fabianrussmann <fabian.russmann@gmail.com>
2012-11-14 19:08:06 +0100

    network and opinion generation works

---
+++ code/main_script.m
@@ -0,0 +1,27 @@
+%%% The main script from which all functions are called
+
+clear;
+
+%Set k and gamma and node number as initial parameters
+k_avg_set = 4;  %wanted average degree
+gamma = 10;     %wanted gamma
+N = 50;        %number of nodes
+
+% Calculate other parameters based on this
+M = k_avg_set*N/2;  %number of edges
+G = N/gamma;    %number of opinions
+
+
+p_connect = (M)/(N*(N-1)); % --> probability of connection given by lecture notes:
+
+
+% Generating graph
+A_sp = random_graph(N, p_connect);  %Generate random graph
+A_adj = full(A_sp);                 %Same graph as an adjacency 
+k_avg = 2*sum(sum(A_adj))/N;        %Compute avg degree to compare with set
+
+
+
+%Generating opinion vector
+%Nx1 vector filled with rand ints from 1 to G with replacement=true
+g = randsample(G,N,true);
---
+++ code/simulation.m
@@ -0,0 +1,11 @@
+function [A_adj, g] = simulation(A_adj, g)
+%SIMULATION Executes the simulation steps in a given network
+%   Takes an adjencency matrix A_adj and an opinion vector g.
+%   Applies iterative rules, alternating opinions and network
+%   When steady state is reached: Returns network and opinion vector.
+
+
+
+
+end
+
