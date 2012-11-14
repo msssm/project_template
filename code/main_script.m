e9f2aad115f8210a9f70e2707f46e3d4c4a4489d
fabianrussmann <fabian.russmann@gmail.com>
2012-11-14 23:15:28 +0100

    simulation function should be working, final testing necessary just to make sure!

--- code/main_script.m
+++ code/main_script.m
@@ -5,7 +5,8 @@ clear;
 %Set k and gamma and node number as initial parameters
 k_avg_set = 4;  %wanted average degree
 gamma = 10;     %wanted gamma
-N = 50;        %number of nodes
+N = 20;        %number of nodes
+phi = 0.0;
 
 % Calculate other parameters based on this
 M = k_avg_set*N/2;  %number of edges
@@ -25,3 +26,7 @@ k_avg = 2*sum(sum(A_adj))/N;        %Compute avg degree to compare with set
 %Generating opinion vector
 %Nx1 vector filled with rand ints from 1 to G with replacement=true
 g = randsample(G,N,true);
+
+
+%Run simulation
+[A_adj, g] = simulation(A_adj, g, N, M, phi);
