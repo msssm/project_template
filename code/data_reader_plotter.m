256de1b2837d0cc45ea385e43eeb7fbe166477ac
fabianrussmann <fabian.russmann@gmail.com>
2012-11-19 22:51:58 +0100

    Generated some data

--- code/data_reader_plotter.m
+++ code/data_reader_plotter.m
@@ -1,17 +1,26 @@
 %%%Reading csv data and plotting it
 
-clear;
 
 phi = 0.1;
-N = 1000;
+N = 3200;
 
-A_adj = csvread(['output_matrix_phi=' num2str(phi) '.csv']);
-g = csvread(['output_opinions_phi=' num2str(phi) '.csv']);
+A_adj = csvread(['output_matrix_phi=' num2str(phi) 'N = ' num2str(N) '.csv']);
+g = csvread(['output_opinions_phi=' num2str(phi) 'N = ' num2str(N) '.csv']);
 
-x = hist(cluster_distr(g), length(g))   %generate histogram of cluster size distribution (fig 2 in paper)
-title(['Histogram at \phi = ' num2str(phi) 'N = ' num2str(N)]);
-set(gca, 'YScale', 'log')
-set(gca, 'XScale', 'log')
+distr = cluster_distr(g);
+
+x = hist(cluster_distr(g), length(g));   %generate histogram of cluster size distribution (fig 2 in paper)
+%title(['Histogram at \phi = ' num2str(phi) 'N = ' num2str(N)]);
+
+loglog(x/sum(x), 'o')
+
+% ph = get(gca,'children');
+% vn = get(ph,'Vertices');
+% vn(:,2) = vn(:,2) + 1;
+% set(ph,'Vertices',vn);
+% 
+% set(gca,'yscale','log')
+% set(gca,'xscale','log')
 
 
 
--- code/main_script.m
+++ code/main_script.m
@@ -5,7 +5,7 @@ clear;
 %Set k and gamma and node number as initial parameters
 k_avg_set = 4;  %wanted average degree
 gamma = 10;     %wanted gamma
-N = 3000;        %number of nodes
+N = 3200;        %number of nodes
 
 % Calculate other parameters based on this
 M = k_avg_set*N/2;  %number of edges
@@ -14,7 +14,7 @@ G = N/gamma;    %number of opinions
 p_connect = (M)/(N*(N-1)); % --> probability of connection given by lecture notes:
 
 
-for phi = 0.1:0.1:0.9
+for phi = [0.1, 0.458, 0.9]
 
     phi
     % Generating graph
@@ -35,8 +35,9 @@ for phi = 0.1:0.1:0.9
     %Run simulation
     [A_adj, g] = simulation(A_adj, g, N, phi);
 
-    csvwrite(['output_matrix_phi=' num2str(phi) 'N = ' num2str(n) '.csv'], A_adj);      %writing "developed" graph to file
-    csvwrite(['output_opinions_phi=' num2str(phi) 'N = ' num2str(n) '.csv'], g);
+    %Write this as built-in matlab functions
+    csvwrite(['output_matrix_phi=' num2str(phi) 'N = ' num2str(N) '.csv'], A_adj);      %writing "developed" graph to file
+    csvwrite(['output_opinions_phi=' num2str(phi) 'N = ' num2str(N) '.csv'], g);
 
     hist(cluster_distr(g), length(g))   %generate histogram of cluster size distribution (fig 2 in paper)
     title(['Histogram at \phi = ' num2str(phi) 'N = ' num2str(N)]);
