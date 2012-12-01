65c643ca337b495bc881011d94d83d52ec21b9ef
fabianrussmann <fabian.russmann@gmail.com>
2012-12-01 16:36:49 +0100

    clustersize_distr now using matrix instead of opinion vector for analysis
    Used a built-in function to find clusters based on sparse matrix.
    description in the function clustersize_distr. The old function based
    on opinion is saved as a backup. The new function is yet to be tested.

--- code/clustersize_distr.m
+++ code/clustersize_distr.m
@@ -1,23 +1,17 @@
-function [s] = clustersize_distr(g)
+function [s] = clustersize_distr(A_sp)
 %CLUSTERSIZE_DISTR returns an unnormalized histogram vector that contains the frequency with
-%which a cluster of certain size occurs in the network. Clusters of zero
-%size, i.e. of non-existent opinions, are disregarded. The function then
-%plots this histogram in a logarithmic plot.
+%which a cluster of certain size occurs in the network. 
 
-n = hist(g,max(g));
-%Renerate histogram data of opinion vector with binning nummber equal to 
-%number of opinions, i.e. at least the maximal value of g. E.g. it could
-%be that there are 50 opinions but the last 3 have gone extinct. Therefore
-%it is sufficient to choose 47 bins to capture each opinion with one bin.
+[number, nodes] = graphconncomp(A_sp)
+%This function returns number, the number of clusters in the network and
+%nodes, which is a vector telling which cluster each node belongs to:
+%Indices are nodes, values are the cluster labels that each node belongs to
 
-nn = n(n~=0);     
-%Remove the zeros (there will be tons of clusters with size zero, but nobody cares)
-%Clusters of zero size are clusters of opinions that do not exist in the
-%consensus state. This step basically prevents a high peak at size zero to
-%occur, when we take hist(n)
+n = hist(nodes,max(nodes));
 
-s=hist(nn,max(nn));   
+s=hist(n,max(n));   
 %Generate histogram of cluster size distribution (see Fig. 2 in paper)
+
 
 end
 
