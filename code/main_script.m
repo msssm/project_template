0eb999fa137674ae07d7e5ca76caaf5790c8ee6a
Stefan Rustler <rustlers@student.ethz.ch>
2012-12-02 20:05:26 +0100

    Optimized data saving into folders
    Now folders with specific N and phi are put into one Data/Subfolder,
    where subfolder is given by current date and time of the start of the
    simulation (serial date number format!).

--- code/main_script.m
+++ code/main_script.m
@@ -4,9 +4,9 @@
 
 clear;
 
-for NN = [1000]              %Iterate over different system sizes (number of nodes)
+for NN = [100 101]              %Iterate over different system sizes (number of nodes)
 
-    for PHI=[0.9]  %Iterate of different probabilities of reconnection
+    for PHI=[0.05 0.06]  %Iterate of different probabilities of reconnection
     %parfor PHI=2:1:3
         %% INITIAL PARAMETERS
 
@@ -15,7 +15,7 @@ for NN = [1000]              %Iterate over different system sizes (number of nod
         k_avg_set = 4;  %wanted average degree
         gamma = 10;     %wanted average number of people per opinion
         N = NN;        %number of nodes
-        ii = 100;         %Number of iterations used for the averaging loop
+        ii = 2;         %Number of iterations used for the averaging loop
 
         phi = PHI;      %Reconnection probability
         %phi = PHI/10;   %Recalculate phi when parfor is active (since parfor requires consecutive int values)
@@ -42,7 +42,7 @@ for NN = [1000]              %Iterate over different system sizes (number of nod
         %Write strings with relevant data for documentation
         str=['N = ',num2str(N),char(10),'k = ',num2str(k_avg_set),char(10),'\gamma = ',num2str(gamma),char(10),'\Phi = ',num2str(phi),char(10),'Runs = ',num2str(ii)]; %String for figure legend
         str2=['N',num2str(N),'k',num2str(k_avg_set),'gamma',num2str(gamma),'Phi',num2str(phi),'Runs',num2str(ii)]; %Shorter string for SAVEDATA
-
+        folder=[num2str(now),'/']; %Name folder to current date and time (serial date number format!) to be created in Data. Folders created for different N and phi will be put in 'folder'. 
 
 
 
@@ -51,9 +51,9 @@ for NN = [1000]              %Iterate over different system sizes (number of nod
         %given by AA_sp.
 
         %%Prepare simulation
-        mkdir('Data/',str2);    %Create a folder for this particular run.
-        csvwrite(['Data/',str2,'/AdjIni.csv'],full(AA_sp));       %Write inital adjacency to file
-        csvwrite(['Data/',str2,'/OpiIni.csv'],gg);                %Write inital opinions to file
+        mkdir(['Data/',folder,'/'],str2);    %Create a folder for this particular run.
+        csvwrite(['Data/',folder,'/',str2,'/AdjIni.csv'],full(AA_sp));       %Write inital adjacency to file
+        csvwrite(['Data/',folder,'/',str2,'/OpiIni.csv'],gg);                %Write inital opinions to file
         s_sum=zeros(1,N);       %Initialize sum for calculating the average of s (cluster size distribution vector)
         s=cell(ii,1);           %Create cell with one matrix stored into cell element for each run. Needed for averaging.
         tt=zeros(ii,1);         %Create vector that contains convergence time for each run.
@@ -62,10 +62,10 @@ for NN = [1000]              %Iterate over different system sizes (number of nod
         char 'Starting simulation:'
         for i=1:ii
             status = ['Run ', num2str(i),' of ', num2str(ii), ' at phi = ', num2str(phi)]       %Some status info printed to screen while simulation is running
-            [A_sp, g,t] = simulation2(AA_sp, gg, N, phi);                     %Return updated connections (A_adj)and opinions (g), always starting from the initial AA-adj and gg!
+            [A_sp, g,t] = simulation2(AA_sp, gg, N, phi);                     %Return upfolderd connections (A_adj)and opinions (g), always starting from the initial AA-adj and gg!
             %Fabian: I commented these for the case of averaging over larger numbers
-            %csvwrite(['Data/',str2,'/AdjRun',num2str(i),'.csv'],full(A_sp));    %Write "developed," i.e. consensus, graph to file
-            %csvwrite(['Data/',str2,'/OpiRun',num2str(i),'.csv'],g);             %Write "developed," i.e. consensus, graph to file
+            %csvwrite(['Data/',folder,'/',str2,'/AdjRun',num2str(i),'.csv'],full(A_sp));    %Write "developed," i.e. consensus, graph to file
+            %csvwrite(['Data/',folder,'/',str2,'/OpiRun',num2str(i),'.csv'],g);             %Write "developed," i.e. consensus, graph to file
             
             %This is now the NEW clustersize_distr which takes the sparse
             %matrix, not the opinion vector!
@@ -82,10 +82,10 @@ for NN = [1000]              %Iterate over different system sizes (number of nod
 
         %% PLOT AND SAVE RESULTS
         s_avg=s_sum/ii;                         %Calculate the average cluster size distribution vector.
-        createfigure(s_avg,str,str2);           %Second argument gives input for legend. Third argument (string) gives directory for the saving the figure.
+        createfigure(s_avg,str,[folder,str2]);           %Second argument gives input for legend. Third argument (string) gives directory for the saving the figure.
         op = find(s_avg, 1, 'last' );           %This data is not absolutely necessary as this variable can be calculated and plotted from the stored data.
         %clear AA_sp A_sp gg g i s_sum ss str t; %Clear all the intermediate or elsewhere-saved variables, before saving all data.
-        save(['Data/',str2,'/AllFinVar.mat']);  %Save all relevant variables into a matlab file.
+        save(['Data/',folder,str2,'/AllFinVar.mat']);  %Save all relevant variables into a matlab file.
 
     end
 end
