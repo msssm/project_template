1b43e81de531547068e79a3b89ff042be0cac1d9
fabianrussmann <fabian.russmann@gmail.com>
2012-12-10 00:24:50 +0100

    generated 0.36 0.37 for N300

--- code/main_script.m
+++ code/main_script.m
@@ -5,7 +5,7 @@ clear;
 %Specify inputs here
 N_input = [300];
 %phi_input = [0.0:0.05:0.35 0.38:0.01:0.53 0.55:0.05:0.95];
-phi_input = [0.51 0.52 0.53];
+phi_input = [0.36 0.37];
 ii = 200;         %Number of iterations used for the averaging loop
 k_avg_set = 4;  %wanted average degree
 gamma = 10;     %wanted average number of people per opinion
