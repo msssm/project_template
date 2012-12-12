138258e8a7d15b56ff8a4a28199c66891474d7b0
fabianrussmann <fabian.russmann@gmail.com>
2012-12-12 12:19:50 +0100

    done with simus

--- code/main_script.m
+++ code/main_script.m
@@ -5,7 +5,7 @@ clear;
 %Specify inputs here
 N_input = [400];
 %phi_input = [0.0:0.05:0.30 0.31:0.01:0.54 0.55:0.05:0.95];
-phi_input = [0.23:0.01:0.24];
+phi_input = [0.32];
 ii = 200;         %Number of iterations used for the averaging loop
 k_avg_set = 4;  %wanted average degree
 gamma = 10;     %wanted average number of people per opinion
