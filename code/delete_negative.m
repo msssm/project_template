ff9127856877cfae28f072aa0b12701240790e82
Huiting Zhang <zhuiting@student.ethz.ch>
2015-11-16 16:06:51 +0100

    MEMO: Further work
    1. How to match the ending point with the starting point(Also car model
    will change the feasibility of a certain household)
    2.After we do all the SOC, we directly find out ones with negative SOC,
    then delete these samples in our table
    3. write a function=FUNC_PLAN(number of plan, SOC, LOCATION), which will
    return several alternative plan(the breaking of charing can be random or
    not)
    4. Output the alternative plan with the same format as Evanglos!

---
+++ code/delete_negative.m
@@ -0,0 +1,9 @@
+%This is to delete the sample with negative SOC state
+
+% Input the data set which will be cleaned
+clear
+load TexasTable table
+
+
+for i=1: height(table)
+    if FUNC_SOC(table,)
