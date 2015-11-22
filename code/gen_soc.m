3b33c12ae8fffdcb2c6ce345e46748ab6fd277f8
Huiting Zhang <zhuiting@student.ethz.ch>
2015-11-22 08:20:51 +0100

    update: using tesla instead

--- code/gen_soc.m
+++ code/gen_soc.m
@@ -6,12 +6,16 @@ load CarModel
 %define parent folder of alternative plan
 parentfolder='../plan151117'; % this is in the same level as "code" file
 HHpool = unique(table(:,{'HOUSEID'}));
-HHpool=HHpool(1:500,:);
+car_index=2; %index means which car we use 1-nissan, 2-tesla
+sample_number=1000; % how many output sample we need
+
+
+HHpool=HHpool(1:sample_number,:);
 SOCmatrix=zeros(height(HHpool),24*60);
 for i=1:height(HHpool)
-    SOCmatrix(i,:)=FUNC_SOC(table,HHpool.HOUSEID(i),model(1,:)); % temporary use Nisaan Leaf as a try
+    SOCmatrix(i,:)=FUNC_SOC(table,HHpool.HOUSEID(i),model(car_index,:)); % temporary use Nisaan Leaf as a try
     if isnan(SOCmatrix(i,1))==0
-        SOCalter=FUNC(SOCmatrix(i,:),model(1,:),FUNC_location(table,HHpool.HOUSEID(i)),3);
+        SOCalter=FUNC(SOCmatrix(i,:),model(car_index,:),FUNC_location(table,HHpool.HOUSEID(i)),3);
         agentfolder=['Agent-',num2str(HHpool.HOUSEID(i)),'_Meter-','1'];
         mkdir(parentfolder, agentfolder)
         open([parentfolder,'',agentfolder,])
