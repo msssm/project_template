7368f8439c1c8cab14c3d96a1ddac84a9314168c
lgulich <lgulich@student.ethz.ch>
2016-12-11 18:27:09 +0100

    Delete adjustCardValue.m

--- code/adjustCardValue.m
+++
@@ -1,9 +0,0 @@
-function [newRandValue] = adjustCardValueP1(sigma, randValue)
-
-    newRandValue=-1;
-    
-    while newRandValue<0 || newRandValue>1
-        newRandValue=sigma*randn+randValue;
-    end
-end
-
