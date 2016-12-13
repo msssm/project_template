afe33b6dcfda6b172a763214af430e86f083772c
colinberner <colinberner@192.168.1.107>
2016-12-13 07:26:54 +0100

    +measure1

--- code/idm4.m
+++ code/idm4.m
@@ -1,28 +1,29 @@
-function result = idm4(t,x,plebmap)
+function result = idm4(t,x,plebmap,iq,dis)
+disturbance = dis;
 %idm returns evaluation of intelligent driver model ODE
 %Simulation Parameters
 L = 100000; %Length of the highway in m
 lcar = 0; %Length of the cars in m
 %Model Parameters
- %desired speed in free traffic
-s0 = 2; %minimum distance to next car
-T = 1.5; %desired time headway to vehicle in front
+%desired speed in free traffic
+s0 = 0.5; %minimum distance to next car
+T = 1; %desired time headway to vehicle in front
 a = 0.3; %maximum acceleration of a car
 b = 3; %comfortable braking deceleration
 delta = 4; %exponent used in equation
 sStar = @(va,dva,dva1) s0 + va*T + va*dva/2/sqrt(a*b); %influence of the following car
 sStar2 = @(va,dva,dva1) s0 + va*2*T + va*dva/2/sqrt(a*b); %influence of the following car
-iq=200;
+
 
 result = zeros(length(x),1);
 Ncars = floor(length(x)/2);
 
 for ii = 1:Ncars
-    if x(ii) > 4000 && x(ii) < 4500
-        v0 = 10;
-    else
-        v0=30;
-    end
+    %  if x(ii) > 4000 && x(ii) < 4500
+    %       v0 = 10;
+    %   else
+    v0=30;
+    %    end
     follow = ii-1;
     if ii > 1 %All but first car
         dva = (x(ii+Ncars) - x(follow + Ncars));
@@ -32,18 +33,18 @@ for ii = 1:Ncars
             dva1 = 0;
         end
         sa = x(follow) - x(ii) - lcar;
- %      if sa < s0
- %          sa = s0;
- %      end
+        %      if sa < s0
+        %          sa = s0;
+        %      end
     else %First car
         dva = x(ii+Ncars);
         sa = L - x(ii);
         dva1 = 0;
-%         if x(ii) > 803.5
-%             sa = L - x(ii);
-%         elseif x(ii) > 800
-%             sa = 2.001;
-%         end
+        %         if x(ii) > 803.5
+        %             sa = L - x(ii);
+        %         elseif x(ii) > 800
+        %             sa = 2.001;
+        %         end
     end
     
     %State Space Equation
@@ -51,23 +52,32 @@ for ii = 1:Ncars
     if not(plebmap(ii))
         result(ii) = x(ii+Ncars);
         result(ii+Ncars) = a*(1 - (x(ii+Ncars)/v0)^delta - (sStar(x(ii+Ncars),dva,dva1)/sa)^2);
-
+        
     else
         result(ii) = x(ii+Ncars);
-        if ii<iq
-            result(ii+Ncars) = min(a*(1 - (x(ii+Ncars)/v0)^delta - (sStar2(x(ii+Ncars),dva,dva1)/sa)^2 - (x(ii+Ncars)-v0)),a);
+        if ii<(iq+5)
+            result(ii+Ncars) = min(a*(1 - (x(ii+Ncars)/v0)^delta - (sStar2(x(ii+Ncars),dva,dva1)/sa)^2),a);% - (x(ii+Ncars)-v0)),a);
         else
-            result(ii+Ncars) = min(a*(1 - (x(ii+Ncars)/v0)^delta - (sStar2(x(ii+Ncars),dva,dva1)/sa)^2 - (x(ii+Ncars)-x(ii+Ncars-iq))),a);
+            result(ii+Ncars) = min(a*(1 - (x(ii+Ncars)/v0)^delta - (sStar2(x(ii+Ncars),dva,dva1)/sa)^2 - (x(ii+Ncars)>x(ii+Ncars-iq))*(x(ii+Ncars)-x(ii+Ncars-iq))),a);
         end
         %result(ii+Ncars) = -10000*(x(ii) - (x(ii+1)+x(ii-1))/2);
+        
     end
-     if result(ii)<0
-         result(ii) = 0;
-     end        
-%     else
-%         result(ii) = 0;
-%         result(ii+Ncars) = 0;
-%     end
+    if ii == 5 && disturbance
+        v02 = @(x) max(30 - (x-15000)*25/200,5);
+        if x(ii) > 15000 && x(ii) < 15500
+            
+            result(ii+Ncars) = a*(1 - (x(ii+Ncars)/v02(x(ii)))^delta - (sStar(x(ii+Ncars),dva,dva1)/sa)^2);
+        end
+        
+    end
+    if result(ii)<0
+        result(ii) = 0;
+    end
+    %     else
+    %         result(ii) = 0;
+    %         result(ii+Ncars) = 0;
+    %     end
     
 end
 
--- code/prototype4.m
+++ code/prototype4.m
@@ -1,39 +1,88 @@
+function [res1,res2] = prototype4(iq,dis)
+
 %Simulation Parameters
 dt = 0.5; %Time Step
 L = 100000; %Length of the highway in m
-Ttot = 500; %Total simultaion time
+Ttot = 1000; %Total simultaion time
 Ncars = 103; %may be prime
 
 %Model Parameters
 v0 = 30; %desired speed in free traffic
-s0 = 2; %minimum distance to next car
-T = 1.5; %desired time headway to vehicle in front
+s0 = 0.5; %minimum distance to next car
+T = 1; %desired time headway to vehicle in front
 a = 0.3; %maximum acceleration of a car
 b = 3; %comfortable braking deceleration
 delta = 4; %exponent used in equation
 sStar = @(va,dva) s0 + va*T + va*dva/2/sqrt(a*b); %influence of the following car
-iq = 200;
+%iq = 200;
 %initial condition and times at which cars are injected into the highway
 %x1,x2,x3,...,v1,v2,...
 x0 = zeros(2*Ncars,1);
 %startTimes = zeros(Ncars,1);
-plebmap = zeros(Ncars,1);
+plebmap = zeros(Ncars,1); %map which of the cars are plebejans
 for ii = 1:Ncars
    x0(ii) = 8*(Ncars-ii); %Starting Position [m]
-   x0(ii+Ncars) = 10 + rand(1)*19; %Starting Velocity [m/s]
+   x0(ii+Ncars) = 29;% + rand(1)*19; %Starting Velocity [m/s]
    plebmap(ii) = mod(ii+floor(iq/2),iq) == 0;
  %  startTimes(ii) = 1.7*ii; %Start Time
 end
 
-f = @(t,x) idm4(t,x,plebmap);
+f = @(t,x) idm4(t,x,plebmap,iq,dis);
 
 
-[TOUT,YOUT] = ode15s(f,[0 Ttot],x0);
+[TOUT,YOUT] = ode45(f,[0 Ttot],x0);
 %subplot(1,2,1);
+[ycol, yrow] = size(YOUT);
+measurement = zeros(3,3); 
+taken = zeros(3,3);
+for ii = 1:ycol
+     if YOUT(ii,1) > 5000 && taken(1,1) == 0
+         measurement(1,1) = TOUT(ii);
+         taken(1,1) = 1;
+     end
+     if YOUT(ii,5) > 5000 && taken(2,1) == 0
+         measurement(2,1) = TOUT(ii);
+         taken(2,1) = 1;
+     end
+     if YOUT(ii,103) > 5000 && taken(3,1) == 0
+         measurement(3,1) = TOUT(ii);
+         taken(3,1) = 1;
+     end
+     if YOUT(ii,1) > 13000 && taken(1,2) == 0
+         measurement(1,2) = TOUT(ii);
+         taken(1,2) = 1;
+     end
+     if YOUT(ii,5) > 13000 && taken(2,2) == 0
+         measurement(2,2) = TOUT(ii);
+         taken(2,2) = 1;
+     end
+     if YOUT(ii,103) > 13000 && taken(3,2) == 0
+         measurement(3,2) = TOUT(ii);
+         taken(3,2) = 1;
+     end
+     if YOUT(ii,1) > 20000 && taken(1,3) == 0
+         measurement(1,3) = TOUT(ii);
+         taken(1,3) = 1;
+     end
+     if YOUT(ii,5) > 20000 && taken(2,3) == 0
+         measurement(2,3) = TOUT(ii);
+         taken(2,3) = 1;
+     end
+     if YOUT(ii,103) > 20000 && taken(3,3) == 0
+         measurement(3,3) = TOUT(ii);
+         taken(3,3) = 1;
+     end
+
+end
+% abstand = [measurement(2,1)-measurement(1,1), ...
+%            measurement(3,1)-measurement(2,1);
+%            measurement(2,2)-measurement(1,2), ...
+%            measurement(3,2)-measurement(2,2);
+%            measurement(2,3)-measurement(1,3), ...
+%            measurement(3,3)-measurement(2,3)] 
+
+res1 = measurement(3,3);
+res2 = measurement(2,3);
 plot(TOUT,YOUT(:,1:Ncars));
 title('position')
-%subplot(1,2,2);
-%plot(TOUT,YOUT(:,Ncars+1:2*Ncars));
-% title('velocity')
-%plot(t,xa(1,:),t,xa(2,:));
-%legend('x','v');
+end
