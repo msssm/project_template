25b7aca7678bde574b414a40b4138e498f303fa4
Luzian Hug <luhug@student.ethz.ch>
2016-12-13 18:37:02 +0100

    Added random Guide Car distribution

---
+++ code/idm_final.m
@@ -0,0 +1,86 @@
+function result = idm_final(t,x,guideMap,dis)
+%IDM function used in simulate.m
+
+%Simulation Parameters
+L = 100000; %Length of the highway in m
+lcar = 0; %Length of the cars in m
+%Model Parameters
+%desired speed in free traffic
+s0 = 0.5; %minimum distance to next car
+T = 1; %desired time headway to vehicle in front
+a = 0.3; %maximum acceleration of a car
+b = 3; %comfortable braking deceleration
+delta = 4; %exponent used in equation
+sStar = @(va,dva,dva1) s0 + va*T + va*dva/2/sqrt(a*b); %influence of the following car
+sStar2 = @(va,dva,dva1) s0 + va*2*T + va*dva/2/sqrt(a*b); %influence of the following car
+v0=30; %Max. speed
+
+result = zeros(length(x),1);
+Ncars = floor(length(x)/2);
+
+for ii = 1:Ncars
+
+
+
+
+    if ii > 1 %All but first car
+        dva = (x(ii+Ncars) - x(ii-1 + Ncars));
+
+        sa = x(ii-1) - x(ii) - lcar;
+        %      if sa < s0
+        %          sa = s0;
+        %      end
+    else %First car
+        dva = x(ii+Ncars);
+        sa = L - x(ii);
+        dva1 = 0;
+        %         if x(ii) > 803.5
+        %             sa = L - x(ii);
+        %         elseif x(ii) > 800
+        %             sa = 2.001;
+        %         end
+    end
+    
+    %State Space Equation
+    %if startTimes(ii) <= t %&& sa > s0 + 0.001
+    if not(guideMap(ii)) %"Normal" cars
+        result(ii) = x(ii+Ncars);
+        result(ii+Ncars) = a*(1 - (x(ii+Ncars)/v0)^delta - (sStar(x(ii+Ncars),dva,dva1)/sa)^2);
+        
+    else %Guide Cars
+        result(ii) = x(ii+Ncars);
+        %Determine preceding guide car
+        k = ii-1;
+        while k>0 && not(guideMap(k))
+            k=k-1;
+        end
+        
+        if k <= 0 %if it is the first guide car
+            result(ii+Ncars) = min(a*(1 - (x(ii+Ncars)/v0)^delta - (sStar2(x(ii+Ncars),dva,dva1)/sa)^2),a);
+        else
+            result(ii+Ncars) = min(a*(1 - (x(ii+Ncars)/v0)^delta - (sStar2(x(ii+Ncars),dva,dva1)/sa)^2 - (x(ii+Ncars)>x(k+Ncars))*(x(ii+Ncars)-x(k+Ncars))),a);
+        end
+        %result(ii+Ncars) = -10000*(x(ii) - (x(ii+1)+x(ii-1))/2);
+        
+    end
+    %Introducing a disturbance
+    if ii == 5 && dis
+        v02 = @(x) max(30 - (x-15000)*25/200,5);
+        if x(ii) > 15000 && x(ii) < 15500
+            
+            result(ii+Ncars) = a*(1 - (x(ii+Ncars)/v02(x(ii)))^delta - (sStar(x(ii+Ncars),dva,dva1)/sa)^2);
+        end
+        
+    end
+    if result(ii)<0
+        result(ii) = 0;
+    end
+    %     else
+    %         result(ii) = 0;
+    %         result(ii+Ncars) = 0;
+    %     end
+    
+end
+
+end
+
