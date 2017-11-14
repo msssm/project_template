c70332a72fcefca8198c760e60155d75c1b03e36
Xiaoyu Sun <binggans@126.com>
2017-11-14 22:42:43 +0100

    added a new simulation_test_xisun in which I tried to run the simulation with batchrunner & created an equipment class (still unfinished)

--- code/sccm/_model.py
+++ code/sccm/_model.py
@@ -1,5 +1,6 @@
 from mesa import Model
 from mesa.time import RandomActivation
+from mesa.datacollection import DataCollector
 from sccm.agents import *
 from sccm.market import Exchange
 
@@ -7,12 +8,16 @@ from sccm.market import Exchange
 class CryptoCurrencyModel(Model):
     """A model with some number of agents."""
     def __init__(self, N):
+        self.running = True
         self.num_agents = N
         self.schedule = RandomActivation(self)
         self.exchange = Exchange(self)
         for i in range(self.num_agents):
             a = RandomTrader(i, self)
             self.schedule.add(a)
+        
+        #tried to extract the price after each iteration (not successful)
+        #self.datacollector = DataCollector(model_reporters={'price': lambda model: model.exchange.p})
 
     def step(self):
         '''Advance the model by one step.'''
