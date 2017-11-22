2396e0ab935a6d5736c4e6a8f8823381adb2eb4e
Z. Wang <wangzuowen1996@outlook.com>
2017-11-22 21:36:31 +0100

    added GUI controls

--- java_code/src/Test.java
+++ java_code/src/Test.java
@@ -1,31 +1,295 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.NumberFormat;
+import java.util.List;
 
-public class Test {
-    public static void main(String[] args) {
-        Simulation simulation = new Simulation(50000, 1, 100000, 500, 20, 0.01);
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
 
-        // Testing the initialization
-        for (int i = 0; i < simulation.matrix.width; i++) {
-            for (int j = 0; j < simulation.matrix.height; j++) {
-                System.out.println("=================== Sector: (" + i + ", " + j + ") =====================");
-                for (Individual individual : simulation.matrix.get(i, j)) {
-                    System.out.println(individual);
-                }
-                System.out.println("");
-            }
-        }
+public class Simulation implements PropertyChangeListener{
 
-        // Testing the efficiency of getNeighbors (MUCH faster than MATLAB!)
-        long start = System.nanoTime();
-//        simulation.matrix.getNeighborsFor(simulation.matrix.get(0, 0).get(0), 10);
-        long end = System.nanoTime();
-        System.out.println(end - start);
+	public double epsilon;
+	public final double mu;
+	public double alpha;
+	public final double numberOfPeople;
+	public final double flockRadius;
+	public final double dt;
 
-        // Testing the efficiency of one timestep
-        start = System.nanoTime();
-        simulation.runOneTimestep();
-        end = System.nanoTime();
-        System.out.println(end - start);
+	private double maxX;
+	private double maxY;
+	private Timer timer;
 
-        simulation.runSimulation();
+	private JButton startButton, pauseButton;
+	private JLabel labelEpsilon, labelAlpha, attribute3; // maybe 5 attributes
+	private JFormattedTextField field1, field2;
+	
+    //Formats to format and parse numbers
+    private NumberFormat format1,format2;
+
+	   
+
+	public PositionMatrix matrix;
+
+	public Simulation(double epsilon, double mu, double alpha, double numberOfPeople, double flockRadius, double dt) {
+		this.epsilon = epsilon;
+		this.mu = mu;
+		this.alpha = alpha;
+		this.numberOfPeople = numberOfPeople;
+		this.flockRadius = flockRadius;
+		this.dt = dt;
+		initializeMatrix();
+
+	}
+
+	private void initializeMatrix() {
+		// TODO: Calculate optimal matrix size and sector size
+		// for now: create some bogus values
+		int tempMatrixSize = 10;
+		int tempSectorSize = 10;
+
+		// The maximum x and y values that an individual can have
+		maxX = tempMatrixSize * tempSectorSize;
+		maxY = tempMatrixSize * tempSectorSize;
+
+		matrix = new PositionMatrix(tempMatrixSize, tempMatrixSize, tempSectorSize);
+
+		for (int i = 0; i < numberOfPeople; i++) {
+			// Generate random coordinates
+			double[] coords = new double[2];
+			coords[0] = Math.random() * tempSectorSize * tempMatrixSize;
+			coords[1] = Math.random() * tempSectorSize * tempMatrixSize;
+
+			// Generate random velocity
+			// TODO: Generate more sensible velocity
+			double[] velocity = new double[] { Math.random() - 0.5, Math.random() - 0.5 };
+			// double[] velocity = new double[] {1, 1};
+
+			// TODO: Decide whether individual is participating
+			boolean isParticipating = false;
+
+			Individual individual = new Individual(coords, velocity, isParticipating);
+
+			// Add individual to appropriate sector
+			matrix.add(individual);
+		}
+	}
+
+	public void runSimulation() {
+		// TODO: Implement runSimulation properly
+
+		// Create a window for the simulation
+		JFrame window = new JFrame("Moshpit Simulation");
+		window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		window.setLayout(new BorderLayout());
+		SimulationPanel panel = new SimulationPanel(500, 500, matrix.getIndividuals(), matrix.width * matrix.sectorSize,
+				matrix.height * matrix.sectorSize);
+		JPanel buttonPanel = new JPanel();
+
+		window.add(panel, BorderLayout.CENTER);
+
+		buttonPanel.setSize(new Dimension(500, 300));
+		buttonPanel.setPreferredSize(new Dimension(500, 300));
+		buttonPanel.setMinimumSize(new Dimension(500, 300));
+		buttonPanel.setBackground(Color.WHITE);
+		
+		buttonPanel.setLayout(new GridLayout(3,2));
+
+		startButton = new JButton("start");
+		pauseButton = new JButton("pause");
+		buttonPanel.add(startButton);
+		buttonPanel.add(pauseButton);
+		
+		
+        //Create the text fields and set them up.
+		labelEpsilon = new JLabel("epsilon");
+		buttonPanel.add(labelEpsilon);
+		
+		
+        field1 = new JFormattedTextField();
+        field1.setValue(new Double(100));
+        field1.setColumns(10);
+        field1.addPropertyChangeListener("value", this);  
+        buttonPanel.add(field1);
+        
+
+        labelAlpha = new JLabel("alpha");
+		buttonPanel.add(labelAlpha);
+		
+        field2 = new JFormattedTextField();
+        field2.setValue(new Double(100));
+        field2.setColumns(10);
+        field2.addPropertyChangeListener("value", this);
+        buttonPanel.add(field2);
+
+       
+		
+		
+		buttonPanel.setVisible(true);
+
+		window.add(buttonPanel, BorderLayout.SOUTH);
+
+		window.pack();
+
+		startButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				timer.start();
+				timer.restart();
+			}
+		});
+
+		pauseButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				timer.stop();
+			}
+		});
+
+		// Run a new frame every 50 milliseconds
+		timer = new Timer(50, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				runOneTimestep();
+				window.repaint();
+			}
+		});
+
+		window.setVisible(true);
+	}
+
+	/* private */ void runOneTimestep() {
+
+		// List of al the individuals in the matrix
+		List<Individual> individuals = matrix.getIndividuals();
+
+		// Iterate over each individual
+		for (Individual individual : individuals) {
+			// Sector where the individual is before updating position
+			PositionMatrix.Sector initialSector = matrix.getSectorForCoords(individual);
+			// Forces acting upon individual
+			double[] F = { 0.0, 0.0 };
+			// Sum of the neighbor velocities
+			double[] sumOverVelocities = { 0.0, 0.0 };
+			// List of neighbors
+			List<Individual> neighbors = matrix.getNeighborsFor(individual, flockRadius);
+			// Position and velocity of individual
+			double[] position = individual.getPosition();
+			double[] velocity = individual.getVelocity();
+			double r0 = individual.radius;
+
+			// =========================== CALCULATION OF THE FORCES
+			// =================================
+			for (Individual neighbor : neighbors) {
+				// Make sure we are not using the individual him/herself
+				if (neighbor == individual) {
+					continue;
+				}
+				double[] positionNeighbor = neighbor.getPosition();
+				double[] velocityNeighbor = neighbor.getVelocity();
+
+				double distance = individual.distanceTo(neighbor);
+
+				// Repulsive force
+				// We only use neighbors within a radius of 2 * r0
+				if (distance < 2 * r0) {
+					F[0] += epsilon * Math.pow((1 - distance / (2 * r0)), 5 / 2) * (position[0] - positionNeighbor[0])
+							/ distance;
+					F[1] += epsilon * Math.pow((1 - distance / (2 * r0)), 5 / 2) * (position[1] - positionNeighbor[1])
+							/ distance;
+				}
+
+				sumOverVelocities[0] += velocityNeighbor[0];
+				sumOverVelocities[1] += velocityNeighbor[1];
+			}
+
+			// Propulsion
+			// TODO: v0 is the "preferred speed", vi is the "instantaneous
+			// speed"
+
+			// Flocking
+			if (!(sumOverVelocities[0] == 0 && sumOverVelocities[1] == 0)) {
+				double norm = norm(sumOverVelocities);
+				F[0] += alpha * sumOverVelocities[0] / norm;
+				F[1] += alpha * sumOverVelocities[1] / norm;
+			}
+
+			// Add noise
+			// TODO: Generate noise
+
+			// ======================= CALCULATE TIMESTEP
+			// ========================
+			// Using the leap-frog method to integrate the differential equation
+			// d^2y/dt^2 = rhs(y)
+
+			// Shifted initial velocity for leap-frog
+			double[] v_temp = new double[2];
+			v_temp[0] = velocity[0] + 0.5 * dt * F[0];
+			v_temp[1] = velocity[1] + 0.5 * dt * F[1];
+
+			// New position of the individual
+			double newX = position[0] + dt * v_temp[0];
+			double newY = position[1] + dt * v_temp[1];
+
+			// New velocity of the individual
+			double newVx = v_temp[0] + dt * F[0] / 2;
+			double newVy = v_temp[1] + dt * F[1] / 2;
+			double[] newV = { newVx, newVy };
+			// Normalize velocity
+			newVx /= norm(newV);
+			newVy /= norm(newV);
+			newVx *= 30;
+			newVy *= 30;
+
+			// Make sure individuals rebound off the edges of the space
+			if (newX < 0 || newX > maxX) {
+				newVx = -newVx;
+				F[0] = -F[0];
+			}
+			if (newY < 0 || newY > maxY) {
+				newVy = -newVy;
+				F[1] = -F[1];
+			}
+
+			// Make sure they don't get stuck in an out-of-bounds area
+			if (newX >= 0 && newX <= maxX)
+				individual.x = newX;
+			if (newY >= 0 && newY <= maxY)
+				individual.y = newY;
+
+			individual.vx = newVx;
+			individual.vy = newVy;
+
+			// Add the individual to the correct sector
+			PositionMatrix.Sector newSector = matrix.getSectorForCoords(individual);
+			if (!newSector.equals(initialSector)) {
+				matrix.removeAndAdd(individual, initialSector, newSector);
+			}
+		}
+	}
+
+	private double norm(double[] vector) {
+		return Math.sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
+	}
+
+	   /** Called when a field's "value" property changes. */
+    public void propertyChange(PropertyChangeEvent e) {
+        Object source = e.getSource();
+        if (source == field1) {
+            epsilon = ((Number)field1.getValue()).doubleValue();
+        } else if (source == field2) {
+            alpha = ((Number)field2.getValue()).doubleValue();
+        } 
+ 
     }
 }
