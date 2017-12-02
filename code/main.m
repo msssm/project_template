6525a49548afc8b34eb94e79ef7de163693bd603
Elisa Wall <elisawall@public-docking-hci-2961.ethz.ch>
2017-12-02 15:49:44 +0100

    Verschönern des Codes.

--- code/main.m
+++ code/main.m
@@ -2,12 +2,34 @@
 %% "Modelling and Simulating of Social Systems with MATLAB"
 %% author: The Opinionators (Elisa Wall, Alexander Stein, Niklas Tidbury)
 
-function [op] = main(T)
-%% Creating the society
+% number of time steps
+T = 50;
 
+% number of society agents
+N = 1000;
+
+% Defining the properties of the society
+u = 0.4;
+mu = 0.1;
+
+% Defining the properties of the extremists
+n0 = 15;
+p0 = 5;
+kappa0 = 0.3;
+n1 = 15;
+p1 = 5;
+kappa1 = 0.3;
+
+with(T, N, u, mu, n0, p0, kappa0, n1, p1, kappa1)
+without(T, N, u, mu)
+
+% The prgram without extremists as given in the paper
+% Input: 
+% Output: histograms
+function without(T, N, u, mu)
+%% Creating the society
 % A society of N SocietyAgents with opinions op in [0,1] 
 % that are randomly normal distributed
-N = 1000;
 op = normrnd(0.5, 1, N, 1); %Elisa: Maybe also equal distri.
 
 % We want to guarantee that all opinions are in [0,1]
@@ -21,24 +43,22 @@ end
 %% Properties of the SocietyAgents
 
 % The threshold u defines when two agents speak/interact with each other
-u = 0.4;
+% u = 0.4; given as an argument
 
 % Mu defines the change of opinion when two agents speak with each other
-%       --> see function mu
+% mu has to be between 0 and 1 to ensure that all opinions are also
+% between 0 and 1
+% mu = 0.1; given as an argument
 
 %% A world without extrimists
-%{
 % The influence of a single agent in a singlte timestep t is defined in the
 % funtion SocietyAgent. We raise up the time steps to T. 
 % In every time step t, every agent has the chance to speak with another.
-
-% Number of time steps T
-T = 100;
 timeGap = 0.01;
 
 for t = 1:T
     for i = 1:N
-        [op0, op1, k] = SingleAgent(op(i), op, u, N);
+        [op0, op1, k] = SingleAgent(op(i), op, u, N, mu);
         op(i) = op0;
         op(k) = op1;
     end
@@ -48,46 +68,73 @@ for t = 1:T
     pause(timeGap)
     drawnow;
 end
-%}
+
+end
+
+% Input:
+% Output: histograms
+% The program with extremists
+function [op] = with(T, N, u, mu, n0, p0, kappa0, n1, p1, kappa1)
+%% Creating the society
+
+% A society of N SocietyAgents with opinions op in [0,1] 
+% that are randomly normal distributed
+% N = 1000; given as argument
+op = normrnd(0.5, 1, N, 1); %Elisa: Maybe also equal distri.
+
+% We want to guarantee that all opinions are in [0,1]
+for i = 1:N
+    while (op(i) > 1 || op(i) < 0)
+        op(i) = normrnd(0.5, 1);
+    end
+    disp(op(i));
+end
+
+%% Properties of the SocietyAgents
+
+% The threshold u defines when two agents speak/interact with each other
+% u = 0.4; given as argument
+
+% Mu defines the change of opinion when two agents speak with each other
+% mu has to be between 0 and 1 to ensure that all opinions are also
+% between 0 and 1
+% mu = 0.1; given as argument
 
 %% Creating the extremists with opinion 0
 
 % number of extremists
-n0 = 15;
-ex0 = 0;
-% number of agents one extremists can reach
-p0 = 5;
+% n0 = 15; given as an argument
+% number of agents one extremist can reach
+% p0 = 5; given as an argument
 % All agents have the same behavior, so we can sum up the influence of all
 %       agents in the number of people that get influenced
 neff0 = p0 * n0;
 
 % An extremist convinces an agent with probability kappa
-kappa0 = 0.3;
+% kappa0 = 0.3; given as an argument
 
 %% Creating the extremists with opinion 0
-n1 = 15;
-% Opinion of the extremists: Since they have all the same opinion, we do
-%       not need an array.
-ex1 = 1;
-% number of agents one extremists can reach
-p1 = 5;
+% n1 = 15; given as an argument
+% number of agents one extremist can reach
+% p1 = 5; given as an argument
 % All agents have the same behavior, so we can sum up the influence of all
 %       agents in the number of people that get influenced
 neff1 = p1 * n1;
 
 % An extremist convince an agent with probability kappa
-kappa1 = 0.3;
+% kappa1 = 0.3; given as an argument
 
 %%% We have the possibility to define n, p or kappa asymmetrically
 
 
 %% A world with extremists
 % Number of time steps T
+timeGap = 0.01;
 
 for t = 1:T
     % For timestep t; the SoicietyAgents play their game
     for i = 1:N
-        [op0, op1, k] = SingleAgent(op(i), op, u, N);
+        [op0, op1, k] = SingleAgent(op(i), op, u, N, mu);
         op(i) = op0;
         op(k) = op1;
     end
@@ -115,38 +162,31 @@ for t = 1:T
             op(k) = 1;
         end
     end
-    %{
+    
     edges = [0 0.1:0.1 0.2:0.2 0.3:0.3 0.4:0.4 0.475:0.475 0.525:0.525 0.6:0.6 0.7:0.7 0.8:0.8 0.9:0.9 1];
     nbin = 50;
     histogram(op, nbin);
     pause(timeGap)
     drawnow; 
-    %}
+    
 end
 end
-%% functions
 
-% - function returns the weighted difference of opinions
-% - implemented as function to enable further development if needed
-% - mu has to be between 0 and 1 to ensure that all opinions are also
-% between 0 and 1
-function [mu] = fmu(op1, op2)
-mu = 0.1*(op2-op1);
-end
 
 % Defining the influence of a single SocietyAgent during one timestep t
 % Input: op0 = opinion of a single agent, op = opinion of the society,
-%       mu and u as described above
+%       mu, u and N as described above
 % Output: new opinion of op0 (opnew0), 
 %       new opinion of a randomly chosen op1 (opnew1) that interacted with
 %       op0 and the position of op1 (pos)
-function [opnew0, opnew1, pos] = SingleAgent(op0, op, u, N)
+function [opnew0, opnew1, pos] = SingleAgent(op0, op, u, N, mu)
 % op0 meets a randomly chosen agent in the society op, called op1
 pos = randi(N);
 op1 = op(pos);
 if abs(op1 - op0) < u
-    opnew0 = op0 + fmu(op0, op1);
-    opnew1 = op1 + fmu(op1, op0);
+    % weighted difference of opinions, weigh is given by mu
+    opnew0 = op0 + mu*(op1-op0);
+    opnew1 = op1 + mu*(op0-op1);
 else
     opnew0 = op0;
     opnew1 = op1;
