c97c7a17169e9284342a19f4b77e8326246171cd
Clemens Giuliani <clemens@inailuig.it>
2017-12-02 12:25:43 +0100

    add initialization and parameters described in paper

--- code/sccm/_model.py
+++ code/sccm/_model.py
@@ -23,16 +23,29 @@ class CryptoCurrencyModel(Model):
             agentType = np.random.choice((RandomTrader, Chartist, Miner))
             a = agentType(self.next_available_id, self)
             self.next_available_id += 1
-            # todo: distribute initial cash/btc according to correct law
-            a.cash_available = 5.
+            # distribute initial cash/btc according to zipf law
+            a.cash_available = numpy.random.zipf(a=1.)
             # a.bitcoin_available = 10.
             self.schedule.add(a)
         # todo: should we generate all traders at beginning? only a few are active
         # todo: distribute initial cash according to zipf law
-        self.datacollector = DataCollector(model_reporters={'price': lambda model: model.exchange.current_price})
+        rep = {'price': lambda model: model.exchange.current_price}
+        rep['nagents'] = lambda model: model.schedule.get_agent_count()
+        rep['nchartist'] = lambda model: [type(a) for a in model.schedule.agents].count(Chartist)
+        rep['ntrader'] = lambda model: [type(a) for a in model.schedule.agents].count(RandomTrader)
+        rep['nminer'] = lambda model: [type(a) for a in model.schedule.agents].count(Miner)
+        
+        self.datacollector = DataCollector(model_reporters=rep)
 
     def step(self):
         '''Advance the model by one step.'''
+        # enter new agents:
+        number_to_enter = Parameters.number_of_traders(self.schedule.time) - self.schedule.get_agent_count()
+        if number_to_enter > 0:
+            to_enter = self.later_traders[-number_to_enter:]
+            self.later_traders = self.later_traders[:-number_to_enter]
+            for a in to_enter:
+                self.schedule.add(a)
         self.datacollector.collect(self)
         self.schedule.step()
         # todo should we do orders as they come in or once a day
@@ -48,3 +61,34 @@ class CryptoCurrencyModel(Model):
             a.bitcoin_available = bitcoin
             self.schedule.add(a)
     # todo: enter agents into the market over time
+    def PaperModel():
+        m = CryptoCurrencyModel(0)
+        m.t_end = 365 * 5
+        number_of_initial_traders = Parameters.number_of_traders(0)  # 160
+        number_of_final_traders = Parameters.number_of_traders(m.t_end)  # ~36000
+        begin_price = 0.0649  # usd per btc
+        begin_cash_richest_trader = 20587
+        begin_bitcoin_value_in_cash_richest_trader = 4117  # in usd
+        begin_bitcoin_richest_trader = begin_bitcoin_value_in_cash_richest_trader/begin_price
+        # total_initial_crypto_cash = 23274
+        for i in range(number_of_initial_traders):
+            kind = Parameters.random_agent_kind(0)
+            a = kind(i, m)
+            a.cash_available = begin_cash_richest_trader/(i+1)
+            a.bitcoin_available = begin_bitcoin_richest_trader/(i+1)
+            m.schedule.add(a)
+        initial_cash_traders_afterwards_zipf_const = 200000
+        later_traders = []
+        for i in range(number_of_initial_traders, number_of_final_traders):
+            kind = Parameters.random_agent_kind(m.t_end)
+            a = kind(i, m)
+            a.cash_available = initial_cash_traders_afterwards_zipf_const/(i**0.6)
+            a.bitcoin_available = 0.
+            later_traders.append(a)
+        np.random.shuffle(later_traders)  # order list randomly
+        m.later_traders = later_traders
+        return m
+
+
+
+        return
--- code/sccm/_parameters.py
+++ code/sccm/_parameters.py
@@ -1,4 +1,7 @@
-from math import exp
+import numpy as np
+
+from math import exp, log
+from sccm.agents import *
 
 
 class Parameters():
@@ -13,3 +16,34 @@ class Parameters():
             return 72.
         else:
             return 36.
+
+    @staticmethod
+    def number_of_traders(t):  # N_t
+        a = 2624.
+        b = 0.002971
+        return round(a * exp(b * (t + 608))/100)  # todo: make factor /100 variable
+
+    @staticmethod
+    def probability_to_be_a_miner(t):  # N_t
+        a = 0.9425
+        b = -0.002654
+        return a * exp(b * (t-1))  # our time starts at 0, paper starts at 1
+
+    probability_to_be_a_random_trader = 0.7  # given it is not a miner
+
+    probability_to_be_a_chartist = 0.3  # given it is not a miner
+
+    @classmethod
+    def random_agent_kind(cls, t):
+        if np.random.random() < cls.probability_to_be_a_miner(t):
+            return Miner
+        elif np.random.random() < cls.probability_to_be_a_random_trader:
+            return RandomTrader
+        else:
+            return Chartist
+
+    @staticmethod
+    def total_number_of_bitcoins(N_traders):
+        b_1 = 0.
+        gamma = 0.
+        return b_1 * ln(N_traders) + gamma
