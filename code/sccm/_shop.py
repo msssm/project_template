2374922b112470f1f9aaf2cc9c98a685a8c9ec98
Clemens Giuliani <clemens@inailuig.it>
2017-12-03 14:58:14 +0100

    add shop; used for stats only at the moment

---
+++ code/sccm/_shop.py
@@ -0,0 +1,8 @@
+class Shop:
+    def __init__(self):
+        self.cash_spent_on_new_hardware = 0.
+        self.cash_spent_on_electricity = 0.
+
+    def prepare_next_step(self):
+        self.cash_spent_on_new_hardware = 0.
+        self.cash_spent_on_electricity = 0.
