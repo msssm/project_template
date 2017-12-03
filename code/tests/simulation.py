f7732c17adc688a608a52beb2c68b7277009167e
Clemens Giuliani <clemens@inailuig.it>
2017-12-03 15:12:48 +0100

    update test simulation

--- code/tests/simulation.py
+++ code/tests/simulation.py
@@ -6,10 +6,7 @@ from tqdm import * #progress bar
 
 
 def main(n_steps):
-    model = CryptoCurrencyModel(0)
-    model.add_agent(Miner, 5000., 100)
-    model.add_agent(RandomTrader, 10., 20)
-    model.add_agent(Chartist, 20., 20)
+    model = CryptoCurrencyModel.PaperModel()
     # todo use batch runner from mesa for MC sim
     for i in tqdm(range(n_steps)):
         model.step()
