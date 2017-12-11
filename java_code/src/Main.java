08a30077c28740e7cda1a4302e04c3acbae1b9ca
hornel <hornel@student.ethz.ch>
2017-12-11 19:33:32 +0100

    Changed config parameters to be uniform for data collection

--- java_code/src/Main.java
+++ java_code/src/Main.java
@@ -3,11 +3,12 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-//        Simulation simulation = new Simulation(2, 10, 700, 600,500, 8, 0.01, 0.5, 6, 1, 3);
+        Simulation simulation = new Simulation(2, 10, 700, 600,500, 8, 0.01, 0.5, 6, 1, 3);
 ////        Timer timer = new Timer(2000, new DataCollector(simulation, "test"));
 //        simulation.runAutomaticSimulation();
 ////        timer.start();
-        AutomaticSimulator test = new AutomaticSimulator();
-        test.run();
+//        AutomaticSimulator test = new AutomaticSimulator();
+//        test.run();
+        simulation.runManualSimulation();
     }
 }
