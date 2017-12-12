ac18dda7daac7b4e4b66328319e3b0e6a64c9409
johannahed <32646545+johannahed@users.noreply.github.com>
2017-12-12 13:42:41 +0100

    timeEvolution plots median instead of average

--- java_code/timeEvolution.py
+++ java_code/timeEvolution.py
@@ -24,40 +24,43 @@ def analyse(test_set):
     t=linspace(0,1,n)
 
     figure()    
-    title('Maximum Danger for given Configuration')
+#    ylim(0,5)
+    title('Maximum Danger for Configuration: ' + test_set)
     
     for j in range(0,m):        #iterating over different seeds
         for k in range(0,n):    #iterating over time steps
             filename='out_%s_%s' %(j,k)
             exec("from %s.%s import *" % (test_set, filename), globals())   #importing data for given seed and time steps
             maxDanger_TimeEvolution[k,j]+=maxDanger
-        plot(t,maxDanger_TimeEvolution[:,j],'--',linewidth=0.2)   #creating plot
-    
-    plot(t,mean(maxDanger_TimeEvolution,axis=1),linewidth=1.5,label='mean for different seeds')
+#        plot(t,maxDanger_TimeEvolution[:,j],'--',linewidth=0.2)   #creating plot
+    y=median(maxDanger_TimeEvolution,axis=1)
+    plot(t,y,linewidth=1.5,label='mean for different seeds')
+
     legend()
-    savefig('maxDanger_timeEvolution.png')
+    savefig(test_set+'_maxDanger_timeEvolution.png')
     show()    
     
     
-    figure()    
-    title('Average Danger for given Configuration')
+    figure()   
+#    ylim(0,1)
+    title('Average Danger for Configuration: '+ test_set)
     
     for j in range(0,m):        #iterating over different seeds
         for k in range(0,n):    #iterating over time steps
             filename='out_%s_%s' %(j,k)
             exec("from %s.%s import *" % (test_set, filename), globals())   #importing data for given seed and time steps
             averageDanger_TimeEvolution[k,j]+=averageDanger
-        plot(t,averageDanger_TimeEvolution[:,j],'--',linewidth=0.2)
-    
-    plot(t,mean(averageDanger_TimeEvolution,axis=1),linewidth=1.5,label='mean for different seeds')
+#        plot(t,averageDanger_TimeEvolution[:,j],'--',linewidth=0.2)
+    y=median(averageDanger_TimeEvolution,axis=1)
+
     legend()
-    savefig('averageDanger_timeEvolution.png')
+    savefig(test_set+'averageDanger_timeEvolution.png')
     show()
 
 if __name__ == '__main__' and __package__ is None:
     import sys, os.path as path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-    from doubleHalfLine.counter import n
+
 
     with open("configs.sim") as f:
         next(f)
