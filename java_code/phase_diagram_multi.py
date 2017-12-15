e1a115cb79cca3fcfa24d24a7e4c8cd46fd59079
elhavron <maichi5@yahoo.com>
2017-12-15 18:44:09 +0100

    density and forces to continuousDangerLevel phase diagram

--- java_code/phase_diagram_multi.py
+++ java_code/phase_diagram_multi.py
@@ -1,46 +1,42 @@
 
-
 import numpy as np
 import matplotlib.pylab as plt
 #from phase_diagram import phaseDiagram
 from scipy import interpolate
 
 # Had trouble importing it from phase_diagram, so just copied it
-def phaseDiagram(x, y, density, F):
+def phaseDiagram(x, y, danger):
 
     # levels to be considerated safe/dangerous
-    safeDensity = 10;
-    density1 = 25;
-    density2 = 40;
+    safeDanger = 0.4
+    danger1 = 0.9
+    danger2 = 1.4
 
-    safeForce = 1000;
-    force1 = 2500;
-    force2 = 4000;
 
     # interpolates the data to get a full grid
     grid_x, grid_y = mgrid[0:99:200j, 0:99:200j]
     points = (x, y)
-    grid_density = interpolate.griddata(points, density, (grid_x, grid_y),method='cubic')
-    grid_F = interpolate.griddata(points, F, (grid_x, grid_y),method='cubic')
+    grid_danger = interpolate.griddata(points, danger, (grid_x, grid_y),method='cubic')
+#    grid_F = interpolate.griddata(points, F, (grid_x, grid_y),method='cubic')
 
     plt.figure()
     # plots density diagram
-    plt.subplot(121)
-    plt.subplots_adjust(wspace=0.5)
-    imD = plt.imshow(grid_density, extent=(0,99,0,99), origin='upper', cmap='hot', vmin=safeDensity, vmax=density2+10)
+#    plt.subplot(121)
+#    plt.subplots_adjust(wspace=0.5)
+    imD = plt.imshow(grid_danger, extent=(0,99,0,99), origin='upper', cmap='hot', vmin=safeDanger, vmax=danger2+0.2)
     plt.axis('off')
-    cbarD = plt.colorbar(imD, fraction=0.046, pad=0.04, orientation='vertical', ticks=[safeDensity, density1, density2])
-    cbarD.ax.set_yticklabels(['Low', 'Medium', 'Death'])
-    plt.title('Density danger')
+    cbarD = plt.colorbar(imD, fraction=0.046, pad=0.04, orientation='vertical', ticks=[safeDanger, danger1, danger2])
+    cbarD.ax.set_yticklabels(['Low', 'Medium', 'High'])
+    plt.title('Danger Level')
 
     # plots force diagram
-    plt.subplot(122)
-    imF = plt.imshow(grid_F, extent=(0,99,0,99), origin='upper', cmap='hot', vmin=safeForce, vmax=force2+1000)
-    plt.axis('off')
-    cbarD = plt.colorbar(imF, fraction=0.046, pad=0.04, orientation='vertical', ticks=[safeForce, force1, force2])
-    cbarD.ax.set_yticklabels(['Low', 'Medium', 'Death'])
-
-    plt.title('Force danger')
+#    plt.subplot(122)
+#    imF = plt.imshow(grid_F, extent=(0,99,0,99), origin='upper', cmap='hot', vmin=safeForce, vmax=force2+1000)
+#    plt.axis('off')
+#    cbarD = plt.colorbar(imF, fraction=0.046, pad=0.04, orientation='vertical', ticks=[safeForce, force1, force2])
+#    cbarD.ax.set_yticklabels(['Low', 'Medium', 'Death'])
+#
+#    plt.title('Force danger')
 
     plt.show()
 
@@ -49,9 +45,8 @@ def phaseDiagram(x, y, density, F):
 # diagram as an average of a set of 'out.py' files
 
 # Average function which look for two equals points
-def average_(points2, d2, f2):
-    global density_
-    global force_
+def average_(points2, d2):
+    global continuousDangerLevel_
     global points
 #    for i in range(len(points2[0])):
 #        for j in range(len(points_[0])):
@@ -64,12 +59,9 @@ def average_(points2, d2, f2):
 #                    points2[0].pop(i)
 #                    points2[1].pop(i)
 #                    i = i-1
-    density_ = np.append(density_, d2)
-    force_ = np.append(force_, f2)
+    continuousDangerLevel_ = np.append(continuousDangerLevel_, d2)
     points_[0] = np.append(points_[0], points2[0])
     points_[1] = np.append(points_[1], points2[1])
-
-
 
 
 # Imports and averages the data-set from the out files
@@ -78,12 +70,10 @@ def analyse2(test_set, time):
     exec(toDo, globals())
 
     global points_
-    global density_
-    global force_
+    global continuousDangerLevel_
     exec("from %s.out_0_0 import *" % (test_set), globals())
     points_ = [x, y]
-    density_ = density
-    force_ = F
+    continuousDangerLevel_ = continuousDangerLevel
     
     for i in range(0, m):
         try:
@@ -92,10 +82,10 @@ def analyse2(test_set, time):
         except ModuleNotFoundError:
             break
             
-        average_((x, y), density, F)
+        average_((x, y), continuousDangerLevel)
         i = i+1
         
-    phaseDiagram(points_[0], points_[1], density_, force_)
+    phaseDiagram(points_[0], points_[1], continuousDangerLevel_)
 
 
 
@@ -110,4 +100,3 @@ if __name__ == '__main__' and __package__ is None:
         for test_set in f:
             time = int(input('Choose time ->    '))
             analyse2(test_set.strip(), time)
-
