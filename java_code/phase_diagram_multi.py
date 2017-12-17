9605dc4bf25cc0ea6715f2ff5012285a257951a9
hornel <hornel@student.ethz.ch>
2017-12-17 22:40:20 +0100

    Cleaned up Python code

--- java_code/phase_diagram_multi.py
+++ java_code/phase_diagram_multi.py
@@ -1,100 +1,78 @@
-
 import numpy as np
 import matplotlib.pylab as plt
-#from phase_diagram import phaseDiagram
 from scipy import interpolate
 
-# Had trouble importing it from phase_diagram, so just copied it
-def phaseDiagram(x, y, danger):
 
-    # levels to be considerated safe/dangerous
-    safeDanger = 0.4
+def phase_diagram(x, y, danger):
+    # levels to be considered safe/dangerous
+    safe_danger = 0.4
     danger1 = 0.9
     danger2 = 1.4
-
 
     # interpolates the data to get a full grid
     grid_x, grid_y = mgrid[0:99:200j, 0:99:200j]
     points = (x, y)
-    grid_danger = interpolate.griddata(points, danger, (grid_x, grid_y),method='cubic')
-#    grid_F = interpolate.griddata(points, F, (grid_x, grid_y),method='cubic')
+    grid_danger = interpolate.griddata(points, danger, (grid_x, grid_y),
+                                       method='cubic')
 
     plt.figure()
-    # plots density diagram
-#    plt.subplot(121)
-#    plt.subplots_adjust(wspace=0.5)
-    imD = plt.imshow(grid_danger, extent=(0,99,0,99), origin='upper', cmap='hot', vmin=safeDanger, vmax=danger2+0.2)
+    # plots danger diagram
+    im_d = plt.imshow(grid_danger, extent=(0, 99, 0, 99), origin='upper',
+                      cmap='hot', vmin=safe_danger, vmax=danger2 + 0.2)
     plt.axis('off')
-    cbarD = plt.colorbar(imD, fraction=0.046, pad=0.04, orientation='vertical', ticks=[safeDanger, danger1, danger2])
-    cbarD.ax.set_yticklabels(['Low', 'Medium', 'High'])
+    cbar_d = plt.colorbar(im_d, fraction=0.046, pad=0.04, orientation='vertical',
+                         ticks=[safe_danger, danger1, danger2])
+    cbar_d.ax.set_yticklabels(['Low', 'Medium', 'High'])
     plt.title('Danger Level')
-
-    # plots force diagram
-#    plt.subplot(122)
-#    imF = plt.imshow(grid_F, extent=(0,99,0,99), origin='upper', cmap='hot', vmin=safeForce, vmax=force2+1000)
-#    plt.axis('off')
-#    cbarD = plt.colorbar(imF, fraction=0.046, pad=0.04, orientation='vertical', ticks=[safeForce, force1, force2])
-#    cbarD.ax.set_yticklabels(['Low', 'Medium', 'Death'])
-#
-#    plt.title('Force danger')
 
     plt.show()
 
 
-# This program shows density's and force's levels in a phase
+# This program shows danger levels in a phase
 # diagram as an average of a set of 'out.py' files
 
-# Average function which look for two equals points
+# Average function which looks for two equals points
 def average_(points2, d2):
-    global continuousDangerLevel_
+    global continuous_danger_level_
     global points
-#    for i in range(len(points2[0])):
-#        for j in range(len(points_[0])):
-#            if points_[0][j] == points2[0][i]:
-#                if points_[1][j] == points2[1][i]:
-#                    density_[j] = 0.6*(density_[j]+d2[i])
-#                    force_[j] = 0.6*(force_[j]+f2[i])
-#                    d2.pop(i)
-#                    f2.pop(i)
-#                    points2[0].pop(i)
-#                    points2[1].pop(i)
-#                    i = i-1
-    continuousDangerLevel_ = np.append(continuousDangerLevel_, d2)
+    continuous_danger_level_ = np.append(continuous_danger_level_, d2)
     points_[0] = np.append(points_[0], points2[0])
     points_[1] = np.append(points_[1], points2[1])
 
 
 # Imports and averages the data-set from the out files
 def analyse2(test_set, time):
-    toDo = "from %s.counter import *" % test_set
-    exec(toDo, globals())
+
+    # Import the data from the out.py files
+    to_do = "from %s.counter import *" % test_set
+    exec(to_do, globals())
 
     global points_
-    global continuousDangerLevel_
+    global continuous_danger_level_
     exec("from %s.out_0_0 import *" % (test_set), globals())
     points_ = [x, y]
-    continuousDangerLevel_ = continuousDangerLevel
-    
+    continuous_danger_level_ = continuousDangerLevel
+
+    # Iterate over the seeds
     for i in range(0, m):
         try:
             filename_ = 'out_%s_%s' % (i, time)
             exec("from %s.%s import *" % (test_set, filename_), globals())
         except ModuleNotFoundError:
             break
-            
+
+        # Average over the seeds
         average_((x, y), continuousDangerLevel)
-        i = i+1
-        
-    phaseDiagram(points_[0], points_[1], continuousDangerLevel_)
 
-
+    phase_diagram(points_[0], points_[1], continuous_danger_level_)
 
 
 if __name__ == '__main__' and __package__ is None:
     import sys, os.path as path
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-    from doubleHalfLine.counter import n
 
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+    # Iterate over all the config files in configs.sim
     with open("configs.sim") as f:
         next(f)
         for test_set in f:
--- java_code/timeEvolution.py
+++ java_code/timeEvolution.py
@@ -1,101 +1,94 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Dec  8 15:31:01 2017
-
-@author: Johanna
-"""
-from numpy import *
 from matplotlib.pylab import *
+from numpy import *
+
+colors = array(
+    ['dodgerblue', 'darkorange', 'g', 'red', 'purple', 'brown', 'violet',
+     'grey', 'y', 'lightskyblue', 'black',
+     'magenta'])
 
 
-colors=array(['dodgerblue','darkorange','g','red','purple','brown','violet','grey','y','lightskyblue','black','magenta'])
+def analyse_median_danger(test_set, i):
+    to_do = "from %s.counter import *" % test_set
 
-def analyse_medianDanger(test_set,i):
-    toDo = "from %s.counter import *" % test_set
+    exec(to_do, globals())
 
-    exec(toDo, globals())
-    
-    medianDanger_TimeEvolution=zeros((n-5,m))
-    t=linspace(0,1,n-5)
+    median_danger_time_evolution = zeros((n - 5, m))
+
+    t = linspace(0, 1, n - 5)
     title('Time Evolution of Average of Median Danger')
-    
-    for j in range(0,m):        #iterating over different seeds
-        for k in range(0,n-5):    #iterating over time steps
-            filename='out_%s_%s' %(j,k)
-            exec("from %s.%s import *" % (test_set, filename), globals())   #importing data for given seed and time steps
-            medianDanger_TimeEvolution[k,j]+=medianDanger
-    y=average(medianDanger_TimeEvolution,axis=1)
-    ylabel('Median Danger'  )
-    ymax=max(y)
-    if(test_set=='control'):
-        linewidth_=2
+
+    for j in range(0, m):  # iterating over different seeds
+        for k in range(0, n - 5):  # iterating over time steps
+            filename = 'out_%s_%s' % (j, k)
+            exec("from %s.%s import *" % (test_set, filename),
+                 globals())  # importing data for given seed and time steps
+            median_danger_time_evolution[k, j] += medianDanger
+    y = average(median_danger_time_evolution, axis=1)
+    ylabel('Median Danger')
+    ymax = max(y)
+    if (test_set == 'control'):
+        linewidth_ = 2
     else:
-        linewidth_=1.2
-    plot(t,y/ymax,colors[i],linewidth=linewidth_,label=test_set)
-#    savefig(test_set+'averageOFmedianDanger_timeEvolution_average.png',dpi=600)
+        linewidth_ = 1.2
+    plot(t, y / ymax, colors[i], linewidth=linewidth_, label=test_set)
 
 
-def analyse_isParticipating(test_set,i):
+def analyse_is_participating(test_set, i):
     toDo = "from %s.counter import *" % test_set
 
     exec(toDo, globals())
 
-    isParticipating_TimeEvolution=zeros(n-5)
-    t=linspace(0,1,n-5)
-    ylim(0,1)
+    is_participating_time_evolution = zeros(n - 5)
+    t = linspace(0, 1, n - 5)
+    ylim(0, 1)
     title('Time Evolution of Participation Rate')
-    for j in range(0,m):        #iterating over different seeds
-        for k in range(0,n-5):    #iterating over time steps
-            filename='out_%s_%s' %(j,k)
-            exec("from %s.%s import *" % (test_set, filename), globals())   #importing data for given seed and time steps
-            isParticipating_TimeEvolution[k]+=sum(isParticipating)/(500*50)
-            
-    if(test_set=='control'):
-        linewidth_=2
-    else:
-        linewidth_=1.2
-        
-    plot(t,isParticipating_TimeEvolution,colors[i],linewidth=linewidth_,label=test_set)
-    ylabel('Participation Rate')
-    
-#    savefig(test_set+'isParticipating_timeEvolution_average.png',dpi=600)
+    for j in range(0, m):  # iterating over different seeds
+        for k in range(0, n - 5):  # iterating over time steps
+            filename = 'out_%s_%s' % (j, k)
+            exec("from %s.%s import *" % (test_set, filename),
+                 globals())  # importing data for given seed and time steps
+            is_participating_time_evolution[k] += sum(isParticipating) / (
+            500 * 50)
 
+    if (test_set == 'control'):
+        linewidth_ = 2
+    else:
+        linewidth_ = 1.2
+
+    plot(t, is_participating_time_evolution, colors[i], linewidth=linewidth_,
+         label=test_set)
+    ylabel('Participation Rate')
 
 
 if __name__ == '__main__' and __package__ is None:
     import sys, os.path as path
+
+    # Make sure we can import the out.py files
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-#is Participating
+    # isParticipating: iterate over the configs listed in configs.sim
     with open("configs.sim") as f:
         next(f)
-        figure(figsize=(7,5))
-        k=0
+        figure(figsize=(7, 5))
+        k = 0
         for test_set in f:
-            
-            analyse_isParticipating(test_set.strip(),k)
-            k+=1
+            analyse_is_participating(test_set.strip(), k)
+            k += 1
 
-
-    legend(prop={'size':8},fancybox=True,ncol=3,loc=1)
+    legend(prop={'size': 8}, fancybox=True, ncol=3, loc=1)
     xlabel('Time')
-    savefig('isParticipating_timeEvolution.png',dpi=600)
-            
-#Median Danger
+    savefig('isParticipating_timeEvolution.png', dpi=600)
+
+    # Median Danger: iterate over the configs listed in configs.sim
     with open("configs.sim") as f:
         next(f)
-        figure(figsize=(7,5))
-        k=0
+        figure(figsize=(7, 5))
+        k = 0
         for test_set in f:
-            
+            analyse_median_danger(test_set.strip(), k)
+            k += 1
 
-            analyse_medianDanger(test_set.strip(),k)
-            k+=1
-
-    legend(prop={'size':8},fancybox=True,ncol=3,loc=1)
+    legend(prop={'size': 8}, fancybox=True, ncol=3, loc=1)
     xlabel('Time')
-    savefig('medianDanger_timeEvolution.png',dpi=600)
-            
-            
-                        
-            
+    savefig('medianDanger_timeEvolution.png', dpi=600)
