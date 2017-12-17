4faecf395690fc7cb6fafa690a9cedd78ed2d690
johannahed <32646545+johannahed@users.noreply.github.com>
2017-12-17 18:40:14 +0100

    Update timeEvolution.py
    entgültige Version

--- java_code/timeEvolution.py
+++ java_code/timeEvolution.py
@@ -4,105 +4,42 @@ Created on Fri Dec  8 15:31:01 2017
 
 @author: Johanna
 """
-
-
-
 from numpy import *
 from matplotlib.pylab import *
 
 
-def analyse_maxDanger(test_set):
-
-    toDo = "from %s.counter import *" % test_set
-
-
-    exec(toDo, globals())
-
-    maxDanger_TimeEvolution=zeros((n-5,m))
-    averageDanger_TimeEvolution=zeros((n,m))
-    
-    t=linspace(0,1,n-5)
-
-    figure()    
-#    ylim(0,5)
-    title('Maximum Danger for Configuration: ' + test_set)
-    
-    for j in range(0,m):        #iterating over different seeds
-        for k in range(5,n):    #iterating over time steps
-            filename='out_%s_%s' %(j,k)
-            exec("from %s.%s import *" % (test_set, filename), globals())   #importing data for given seed and time steps
-            maxDanger_TimeEvolution[k,j]+=maxDanger
-#        plot(t,maxDanger_TimeEvolution[:,j],'--',linewidth=0.2)   #creating plot
-    y=median(maxDanger_TimeEvolution,axis=1)
-    plot(t,y/((y)),linewidth=1.5,label='mean for different seeds')
-
-    legend()
-    savefig(test_set+'_maxDanger_timeEvolution.png')
-    show()    
-    
-def anayse_averageDanger(test_set):
-    toDo = "from %s.counter import *" % test_set
-
-
-    exec(toDo, globals())
-
-    averageDanger_TimeEvolution=zeros((n,m))
-    
-    t=linspace(0,1,n)
-    figure()   
-#    ylim(0,1)
-    title('Median of Average Danger for Configuration: '+ test_set)
-    
-    for j in range(0,m):        #iterating over different seeds
-        for k in range(0,n):    #iterating over time steps
-            filename='out_%s_%s' %(j,k)
-            exec("from %s.%s import *" % (test_set, filename), globals())   #importing data for given seed and time steps
-            averageDanger_TimeEvolution[k,j]+=averageDanger
-#        plot(t,averageDanger_TimeEvolution[:,j],'--',linewidth=0.2)
-    y=median(averageDanger_TimeEvolution,axis=1)
-    plot(t,y,linewidth=1.5,label='mean for different seeds')
-    legend()
-    savefig(test_set+'medianOfMeanDanger_timeEvolution.png')
-    show()
-
 def analyse_medianDanger(test_set):
     toDo = "from %s.counter import *" % test_set
 
-
     exec(toDo, globals())
-    medianDanger_TimeEvolution=zeros((n-5,m))
     
+    medianDanger_TimeEvolution=zeros((n-5,m))
     t=linspace(0,1,n-5)
-#    figure(x)   
-#    ylim(0,1)
-    title('Average of Median Danger for Configuration: '+ test_set)
-    print(m,n-5,test_set)
+    title('Time Evolution of Average of Median Danger')
     
     for j in range(0,m):        #iterating over different seeds
         for k in range(0,n-5):    #iterating over time steps
             filename='out_%s_%s' %(j,k)
             exec("from %s.%s import *" % (test_set, filename), globals())   #importing data for given seed and time steps
             medianDanger_TimeEvolution[k,j]+=medianDanger
-#        plot(t,averageDanger_TimeEvolution[:,j],'--',linewidth=0.2)
     y=average(medianDanger_TimeEvolution,axis=1)
+    ylabel('Median Danger'  )
     ymax=max(y)
-    plot(t,y/ymax,linewidth=1.5,label='mean for different seeds')
-    legend(prop={'size':5},fancybox=True)
+    if(test_set=='control'):
+        linewidth_=3
+    else:
+        linewidth_=1.2
+    plot(t,y/ymax,linewidth=linewidth_,label=test_set)
 #    savefig(test_set+'averageOFmedianDanger_timeEvolution_average.png',dpi=600)
-#    show()
 
 
-def analyse_isPartisipating(test_set):
+def analyse_isParticipating(test_set):
     toDo = "from %s.counter import *" % test_set
-
 
     exec(toDo, globals())
 
-    
     isParticipating_TimeEvolution=zeros(n-5)
-    
     t=linspace(0,1,n-5)
-  
     ylim(0,1)
     title('Time Evolution of Participation Rate')
     for j in range(0,m):        #iterating over different seeds
@@ -110,28 +47,43 @@ def analyse_isPartisipating(test_set):
             filename='out_%s_%s' %(j,k)
             exec("from %s.%s import *" % (test_set, filename), globals())   #importing data for given seed and time steps
             isParticipating_TimeEvolution[k]+=sum(isParticipating)/(500*50)
-#        plot(t,isParticipating_TimeEvolution[:,j],'--',linewidth=0.2)
-    plot(t,isParticipating_TimeEvolution,linewidth=1.5,label=test_set)
-    legend(prop={'size':5},fancybox=True)
+            
+    if(test_set=='control'):
+        linewidth_=3
+    else:
+        linewidth_=1.2
+        
+    plot(t,isParticipating_TimeEvolution,linewidth=linewidth_,label=test_set)
+    ylabel('Participation Rate')
+#    savefig(test_set+'isParticipating_timeEvolution_average.png',dpi=600)
+
 
 
 if __name__ == '__main__' and __package__ is None:
     import sys, os.path as path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-
+#is Participating
     with open("configs.sim") as f:
         next(f)
-        figure()
+        figure(figsize=(7,5))
         for test_set in f:
-            
-            analyse_isPartisipating(test_set.strip())
-        savefig('isPArticipating_TimeEvolution.png',dpi=600)
-            
-            
-                        
-#            analyse_medianDanger(test_set.strip())
-#        savefig('MedianDangerComparison.png',dpi=600)
-            
 
-        
+            analyse_isParticipating(test_set.strip())
+
+    legend(prop={'size':8},fancybox=True,ncol=3,loc=1)
+    xlabel('Time')
+    savefig('isParticipating_timeEvolution.png',dpi=600)
+            
+#Median Danger
+    with open("configs.sim") as f:
+        next(f)
+        figure(figsize=(7,5))
+        for test_set in f:
+
+            analyse_medianDanger(test_set.strip())
+
+    legend(prop={'size':8},fancybox=True,ncol=3,loc=1)
+    xlabel('Time')
+    savefig('medianDanger_timeEvolution.png',dpi=600)
+            
